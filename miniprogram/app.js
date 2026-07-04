0ac13173a0712d2962c1b920deb09555f06dc3a1
39 <yuii3334oqw@gmail.com>
2026-07-04 15:48:05 +0800

    style(小程序): 内嵌像素/手绘字体恢复网页版观感;补 --pix 变量;去掉首页NEW;修复 room.json 无效配置

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,28 @@
 const CLOUD_ENV = require('./utils/cloudenv');
+const FONT_PIXELIFY = require('./utils/font-pixelify');
+const FONT_GRIFFY = require('./utils/font-griffy');
+
 App({
   onLaunch() {
     if (wx.cloud) {
       wx.cloud.init({ env: CLOUD_ENV, traceUser: true });
     }
+    this.loadFonts();
+  },
+  // 网页版同款可爱字体:base64 内嵌,无需网络/云存储,真机与工具都可用
+  loadFonts() {
+    const load = (family, b64) => {
+      wx.loadFontFace({
+        global: true,
+        family,
+        source: 'url("data:font/ttf;base64,' + b64 + '")',
+        scopes: ['webview'],
+        success: () => console.log('[font] loaded:', family),
+        fail: (e) => console.warn('[font] load fail:', family, e)
+      });
+    };
+    load('Pixelify Sans', FONT_PIXELIFY);
+    load('Griffy', FONT_GRIFFY);
   },
   globalData: {}
 });
