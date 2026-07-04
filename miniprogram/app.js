c1eeb9799657c70ff1d68fda24e64c225ea8d3dc
39 <yuii3334oqw@gmail.com>
2026-07-04 16:32:55 +0800

    style(小程序): 像素SVG图标替换emoji;牌面改像素字体并移除Griffy(-278KB);首页花色装饰+卡片角像素点

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,5 @@
 const CLOUD_ENV = require('./utils/cloudenv');
 const FONT_PIXELIFY = require('./utils/font-pixelify');
-const FONT_GRIFFY = require('./utils/font-griffy');
 
 App({
   onLaunch() {
@@ -9,20 +8,16 @@ App({
     }
     this.loadFonts();
   },
-  // 网页版同款可爱字体:base64 内嵌,无需网络/云存储,真机与工具都可用
+  // 网页版同款像素字体:base64 内嵌,无需网络/云存储
   loadFonts() {
-    const load = (family, b64) => {
-      wx.loadFontFace({
-        global: true,
-        family,
-        source: 'url("data:font/ttf;base64,' + b64 + '")',
-        scopes: ['webview'],
-        success: () => console.log('[font] loaded:', family),
-        fail: (e) => console.warn('[font] load fail:', family, e)
-      });
-    };
-    load('Pixelify Sans', FONT_PIXELIFY);
-    load('Griffy', FONT_GRIFFY);
+    wx.loadFontFace({
+      global: true,
+      family: 'Pixelify Sans',
+      source: 'url("data:font/ttf;base64,' + FONT_PIXELIFY + '")',
+      scopes: ['webview'],
+      success: () => console.log('[font] loaded: Pixelify Sans'),
+      fail: (e) => console.warn('[font] load fail:', e)
+    });
   },
   globalData: {}
 });
